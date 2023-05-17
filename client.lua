efb328530c2b38a9e0e4ea15ceeec8a5d0179f71
Arckant <108737908+Arckant@users.noreply.github.com>
2023-05-17 22:48:12 +0200

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,28 +1,15 @@
-RegisterCommand('anim', function(player, source)
-  local dictName = 'hands@out@generic@ps'
-  local animName = 'clip'
-  print('1')
-  -- Request animation dictionary.
-  RequestAnimDict(dictName)
-  while not HasAnimDictLoaded(dictName) do
-    Wait(1)
-  end
-
-  -- Play animation on player ped.
-  local playerPed = PlayerPedId()
-  TaskPlayAnim(playerPed, dictName, animName, 4.0, 4.0, -1, 1, 0.0)
-
-  -- Unload animation dictionary.
-  RemoveAnimDict(dictName)
-end, false)
-
 player = GetPlayerPed()
 emotes = {
-          'hands@out@generic@ds', 
-          'hands@out@generic@ps', 
-          'hands@out@low@ds', 
-          'hands@out@low@ps'}
-
+  'hands@out@generic@ds', 
+  'hands@out@generic@ps', 
+  'hands@out@low@ds', 
+  'hands@out@low@ps'}
+clips = {
+  'hands@out@generic@ds@clip', 
+  'hands@out@generic@ps@clip', 
+  'hands@out@low@ds@clip', 
+  'hands@out@low@ps@clip'}
+  
 Citizen.CreateThread(function()
   while true do
     if IsControlPressed(1, 289) then
@@ -31,10 +18,21 @@ Citizen.CreateThread(function()
 
     if IsControlJustPressed(1, 288) then
       if IsPedSittingInAnyVehicle(player) then
-        TaskPlayAnim(player, emotes[anim_type()], 'clip', 4.0, 4.0, -1, 1, 0.0)
+        RequestAnimDict(emotes[anim_type()])
+        while not HasAnimDictLoaded(emotes[anim_type()]) do
+          Wait(1)
+        end
+        TaskPlayAnim(player, emotes[anim_type()], clips[anim_type()], 4.0, 4.0, -1, 1, 0.0)
+        RemoveAnimDict(emotes[anim_type()])
+        TriggerEvent('chat:addMessage', {
+          color = { 255, 0, 0},
+          multiline = true,
+          args = {emotes[anim_type()], clips[anim_type()]}
+        })
       end
     end
-    Citizen.Wait(0)
+    RemoveAnimDict(emotes[anim_type()])
+    Citizen.Wait(1)
   end
 end)
 
@@ -42,22 +40,22 @@ end)
 
 function anim_type()
   if player == GetPedInVehicleSeat(GetVehiclePedIsIn(player), -1) then
-    if GetVehicleClass(GetVehiclePedIsIn(player)) == 'super' then
+    if GetVehicleClass(GetVehiclePedIsIn(player)) == 7 then
       return 3;
     end
     return 1;
   elseif player == GetPedInVehicleSeat(GetVehiclePedIsIn(player), 0) then
-    if GetVehicleClass(GetVehiclePedIsIn(player)) == 'super' then
+    if GetVehicleClass(GetVehiclePedIsIn(player)) == 7 then
       return 4;
     end
     return 2;
   elseif player == GetPedInVehicleSeat(GetVehiclePedIsIn(player), 1) then
-    if GetVehicleClass(GetVehiclePedIsIn(player)) == 'super' then
+    if GetVehicleClass(GetVehiclePedIsIn(player)) == 7 then
       return 3
     end
     return 1;
   elseif player == GetPedInVehicleSeat(GetVehiclePedIsIn(player), 2) then
-    if GetVehicleClass(GetVehiclePedIsIn(player)) == 'super' then
+    if GetVehicleClass(GetVehiclePedIsIn(player)) == 7 then
       return 4;
     end
     return 2;
