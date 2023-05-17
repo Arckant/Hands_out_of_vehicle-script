9021f45313b91d49f26e82c2f8d862d2e4b80255
Arckant <108737908+Arckant@users.noreply.github.com>
2023-05-17 20:41:22 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,6 +1,6 @@
 version '1.0.0'
 author 'Arckant#0815'
-repository 'https://github.com/citizenfx/cfx-server-data'
+repository 'https://github.com/Arckant/Hands_out_of_vehicle-script'
 
 resource_type 'client_script' { name = 'Hands_out_of_vehicle' }
 
