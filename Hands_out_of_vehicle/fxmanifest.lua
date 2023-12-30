e74085cba1c2dd236e204621629d6823bcf31db1
Arckant <108737908+Arckant@users.noreply.github.com>
2023-12-30 10:18:07 +0100

    Add files via upload

---
+++ Hands_out_of_vehicle/fxmanifest.lua
@@ -0,0 +1,10 @@
+version '1.1.0'
+author 'Arckant#0815'
+repository 'https://github.com/Arckant/Hands_out_of_vehicle-script'
+
+resource_type 'client_script' { name = 'Hands_out_of_vehicle' }
+
+client_script 'Hands_out_of_vehicle'
+
+game 'gta5'
+fx_version 'cerulean'
