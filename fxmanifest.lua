16a468875aeae915c6296b2a86d4a218632a063f
Arckant <108737908+Arckant@users.noreply.github.com>
2023-12-30 11:02:01 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,7 +4,7 @@ repository 'https://github.com/Arckant/Hands_out_of_vehicle-script'
 
 resource_type 'client_script' { name = 'Hands_out_of_vehicle' }
 
-client_script 'client.lua'
+client_script 'Hands_out_of_vehicle.lua'
 
 game 'common'
 fx_version 'adamant'
