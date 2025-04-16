7158de5cd3abd7de1eadff7134993a8877aad28a
Arckant <108737908+Arckant@users.noreply.github.com>
2025-04-16 15:38:08 +0200

    Update fxmanifest.lua

--- Hands_out_of_vehicle/fxmanifest.lua
+++ Hands_out_of_vehicle/fxmanifest.lua
@@ -1,10 +1,10 @@
 version '1.1.0'
-author 'Arckant#0815'
+author 'arckant'
 repository 'https://github.com/Arckant/Hands_out_of_vehicle-script'
 
 resource_type 'client_script' { name = 'Hands_out_of_vehicle' }
 
-client_script 'Hands_out_of_vehicle'
+client_script 'client.lua'
 
 game 'gta5'
 fx_version 'cerulean'
